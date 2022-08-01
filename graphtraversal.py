bbda43928dcfe3d96fc9c6ab8b67d19a5370990e
QiuDylan <100895454+QiuDylan@users.noreply.github.com>
2022-08-01 07:56:40 -0700

    Add files via upload

--- graphtraversal.py
+++ graphtraversal.py
@@ -52,14 +52,57 @@ class Solution:
         # return type: int (shortest path as an int)
 
         # TODO: Write code below to return an int with the solution to the prompt
-        return 190 
+        
+        graph['Finish'] = {}
+        unvisited_nodes = []
+        nodes = []
+        for node,_ in graph.items ():
+            unvisited_nodes.append (node)
+            nodes.append (node)
+        shortest_path = {}
+        previous_nodes= {}
+        max_value = 10000
+        for node in unvisited_nodes:
+            shortest_path[node] = max_value
+        shortest_path["Start"] = 0
+        while unvisited_nodes:
+            current_min_node = None
+            for node in unvisited_nodes: # Iterate over the nodes
+                if current_min_node == None:
+                    current_min_node = node
+                elif shortest_path[node] < shortest_path[current_min_node]:
+                    current_min_node = node
+# The code block below retrieves the current node's neighbors and updates their distan
+            neighbors = []
+            for next_node in graph[current_min_node]:
+                neighbors.append(next_node)
+            for neighbor in neighbors:
+                tentative_value = shortest_path[current_min_node] + graph[current_min_node][neighbor]
+                if tentative_value < shortest_path[neighbor]:
+                    shortest_path[neighbor] = tentative_value
+                    previous_nodes[neighbor] = current_min_node
+
+
+            unvisited_nodes.remove(current_min_node)
+
+
+
+
         pass
         
 
 def main():
     tc1 = Solution()
 
-    graph = {}
+    graph = { 'Start': {'Invisible Maze': 15, 'The Labyrinth': 20}, 
+'Invisible Maze': {'Park Walk': 45}, 
+'Ice Valley': {'Tower of Doom': 45, 'Sloped Madness': 85}, 
+'The Labyrinth': {'Ice Valley': 45, 'Sloped Madness': 155}, 
+'Tower of Doom': {'Cone Slalom': 10, 'Ice Valley': 45}, 
+'Park Walk': {'Tower of Doom': 45}, 
+'Cone Slalom': {'Sloped Madness': 15, 'Street Dodge': 30}, 
+'Street Dodge': {'Finish': 70}, 
+'Sloped Madness': {'Finish': 60}, 'Finish': {}}
     nodes = input().split(",")
     nodes[len(nodes) - 1] = nodes[len(nodes) - 1].strip()
     for i in range (0, len(nodes) - 1):
